f2215e844b8e2ed082fa89732d24c1efff21ab3d
Charles Abonyi <abonyicharles02@gmail.com>
2026-07-06 14:50:07 +0100

    Created craps game javascript module

---
+++ crapsgame/crapsgame.js
@@ -0,0 +1,19 @@
+// HTML Elements IDs
+const crapsUsernameInput = "craps-username-input"
+const crapsRegistrationPane = "craps-registration-pane"
+const crapsMainSection = "craps-main-section"
+
+function registerCrapsPlayer () {
+    let crapsUsername = document.getElementById(crapsUsernameInput).value
+    alert("Got: " + crapsUsername)
+    removeRegistrationPane()
+    showMainGameSection()
+}
+
+function removeRegistrationPane() {
+    document.getElementById(crapsRegistrationPane).style.display = "none"
+}
+
+function showMainGameSection() {
+    document.getElementById(crapsMainSection).style.display = "block"
+}
