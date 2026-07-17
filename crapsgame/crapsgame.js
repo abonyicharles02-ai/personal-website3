741394b72651742ae7ac3c1fe345d8b0f4fade56
Charles Abonyi <abonyicharles02@gmail.com>
2026-07-17 20:29:51 +0100

    Added craps game stats section

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -1,10 +1,20 @@
+// Craps Main Data
+let crapsUsername = ""
+
+// Craps Game Settings
+const startingMoney = 1000
+const startingRounds = 0
+
 // HTML Elements IDs
 const crapsUsernameInput = "craps-username-input"
 const crapsRegistrationPane = "craps-registration-pane"
 const crapsMainSection = "craps-main-section"
+const crapsStatsUsername = "craps-stats-username"
+const crapsStatsMoney = "craps-stats-money"
+const crapsStatsRounds = "craps-stats-rounds"
 
 function registerCrapsPlayer () {
-    let crapsUsername = document.getElementById(crapsUsernameInput).value
+    crapsUsername = document.getElementById(crapsUsernameInput).value
 
     // Username validaton check
     let firstCharIsDigitRegex = /^[0-9]|[^a-zA-Z0-9_]/g
@@ -13,9 +23,9 @@ function registerCrapsPlayer () {
     } else { 
         removeRegistrationPane()
         showMainGameSection()
+        setupFirstRound()
     }
 }
-
 
 function removeRegistrationPane() {
     document.getElementById(crapsRegistrationPane).style.display = "none"
@@ -24,3 +34,18 @@ function removeRegistrationPane() {
 function showMainGameSection() {
     document.getElementById(crapsMainSection).style.display = "block"
 }
+
+function setupFirstRound() {
+    document.getElementById("craps-stats-username").innerHTML = crapsUsername
+    setMoney(startingMoney)
+    setRounds(startingRounds)
+}
+
+function setMoney (money) {
+    document.getElementById(crapsStatsMoney).innerHTML = money
+}
+
+function setRounds (rounds) {
+    document.getElementById(crapsStatsRounds).innerHTML = rounds
+}
+
